07fcc7d2f3cd6f40c93a9db8f269fc5ee4f8c470
Declan200 <declan2317@hotmail.com>
2020-09-26 19:43:53 +1000

    Update post API (keyword search)

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -1,41 +1,66 @@
 const db = require("../config/setup.js");
+const post = require("../routes/post.js");
+const { findAll } = require("./user.js");
 const Post = db.posts;
 const postRewards = db.postRewards;
 const Op = db.Sequelize.Op;
 
 module.exports = {
 
-    async findOne(req, res){
-        try{
+    async findOne(req, res) {
+        try {
             const post = await Post.findAll({
-                where:{
+                where: {
                     post_id: req.params.id
                 }
             });
-            if (post.length != 0){
-                res.status(200).send({'post':post});
+            if (post.length != 0) {
+                res.status(200).send({ 'post': post });
+            } else {
+                res.status(404).send({ "message": "Post not found" });
             }
-            else{
-                res.status(404).send({"message":"Post not found"});
-            }
-        }
-        catch(e){
+        } catch (e) {
             console.log(e);
             res.status(500).send(e);
         }
     },
+    async findAll(req, res) {
+        try {
+            const keyword = req.query.keyword
+            const post = await Post.findAll({
+                where: {
+                    title: {
+                        [Op.like]: '%' +
+                            keyword + '%'
+                    }
+                }
 
-    async createPost(req,res) {
+            })
+            if (post.length == 0) {
+                res.status(404).send({ "message": "Post not found!" });
+            } else {
+                res.status(200).send({
+                    'post': post
+                });
+            }
+        } catch (e) {
+            res.status(500).send(e);
+            console.log(e)
+        }
+
+    },
+
+    async createPost(req, res) {
         try {
             const inputPost = {
-                added_by : req.body.post.added_by,
-                title : req.body.post.title,
-                description : req.body.post.description
+                added_by: req.body.post.added_by,
+                title: req.body.post.title,
+                description: req.body.post.description
             };
             const post = await Post.create(inputPost);
-            
+
             var rewards = req.body.reward;
-            for(i=0; i<rewards.length; i++){
+            for (i = 0; i < rewards.length; i++) {
                 const inputRewards = {
                     post_id: post.post_id,
                     user_id: req.body.post.added_by,
@@ -44,13 +69,12 @@ module.exports = {
                 };
                 const rewardPost = postRewards.create(inputRewards);
             }
-            
+
             res.status(201).send({
                 "message": "Post successfully created",
                 "post": post
             });
-        }
-        catch(e){
+        } catch (e) {
             console.log(e);
             res.status(400).send(e);
         }
--- app/routes/post.js
+++ app/routes/post.js
@@ -2,9 +2,10 @@ module.exports = app => {
     const posts = require("../controllers/post.js");
 
     var router = require("express").Router();
-    
+
     router.get("/:id", posts.findOne);
+    router.get("/", posts.findAll);
     router.post("/", posts.createPost);
-    
+
     app.use('/api/posts', router);
 };
